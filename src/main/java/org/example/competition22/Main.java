0f9a692dc4a3bd224bdfd0c68c3d001a6b192f1e
Konstantin Petrov <kpetrov@ripe.net>
2022-02-07 10:04:35 +0100

    Add simple http server implementation

--- src/main/java/org/example/competition22/Main.java
+++ src/main/java/org/example/competition22/Main.java
@@ -1,7 +1,34 @@
 package org.example.competition22;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode response = mapper.createObjectNode();
+            response.put("message", "Hello World!");
+            String responseString = mapper.writeValueAsString(response);
+            exchange.sendResponseHeaders(200, responseString.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(responseString.getBytes());
+            os.close();
+        }
     }
 }
