546b473a92b6d3afadd54d7ebcd57001fc01ed2b
Oleg Muravskiy <oleg@ripe.net>
2022-02-17 16:42:34 +0100

    v 0.11 stop eating

--- src/main/java/org/example/competition22/logic/FoodSearch.java
+++ src/main/java/org/example/competition22/logic/FoodSearch.java
@@ -7,9 +7,13 @@ import org.example.competition22.data.MoveRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.OptionalInt;
 
 public class FoodSearch {
     public static void searchFood(MoveRequest request, Map<Direction, Integer> directions) {
+        final OptionalInt maxLength = request.board.snakes.stream().mapToInt(s -> s.length).max();
+        if (request.you.length >= maxLength.orElse(Integer.MAX_VALUE)) return;
+
         Map<FoodDirection, Integer> weights = new HashMap<>();
         var longestPath = request.board.height + request.board.width;
 
