9e788b0e25d4db6c75e9a3c02949e2b6dd940e16
Konstantin Petrov <kpetrov@ripe.net>
2022-02-17 12:59:27 +0100

    Simplify

--- src/main/java/org/example/competition22/logic/FoodSearch.java
+++ src/main/java/org/example/competition22/logic/FoodSearch.java
@@ -39,15 +39,10 @@ public class FoodSearch {
     }
 
     private static class FoodDirection {
-        private List<Move> moves = new java.util.ArrayList<>();
+        private final List<Move> moves = new java.util.ArrayList<>();
         private int distance;
 
         public FoodDirection() {
-        }
-
-        public FoodDirection(List<Move> moves) {
-            this.moves = moves;
-            this.distance = moves.stream().mapToInt(Move::getDistance).sum();
         }
 
         public void add(Move move) {
