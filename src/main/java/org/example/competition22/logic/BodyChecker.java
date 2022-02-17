1b87e979f8dd854437ccb1c9d2de2bdd1efec192
Oleg Muravskiy <oleg@ripe.net>
2022-02-17 13:42:11 +0100

    Exclude our tail

--- src/main/java/org/example/competition22/logic/BodyChecker.java
+++ src/main/java/org/example/competition22/logic/BodyChecker.java
@@ -4,13 +4,16 @@ import org.example.competition22.data.Coordinate;
 import org.example.competition22.data.Direction;
 import org.example.competition22.data.MoveRequest;
 
+import java.util.List;
 import java.util.Map;
 
 public class BodyChecker {
     public static void check(MoveRequest request, Map<Direction, Integer> directions) {
         for (Direction direction : Direction.values()) {
             var possibleMove = Coordinate.getNextCoordinate(request.you.head, direction);
-            for (Coordinate coordinate : request.you.body) {
+            List<Coordinate> body = request.you.body;
+            if (body.size() > 1) body = body.subList(0, body.size()-1);
+            for (Coordinate coordinate : body) {
                 if (coordinate.equals(possibleMove)) {
                     directions.put(direction, 0);
                 }
