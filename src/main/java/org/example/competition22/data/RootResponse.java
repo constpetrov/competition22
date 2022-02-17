f45a609a7f8d861f9500bf5c3608f8322037e91c
Konstantin Petrov <kpetrov@ripe.net>
2022-02-17 11:02:31 +0100

    Add snake customisation

--- src/main/java/org/example/competition22/data/RootResponse.java
+++ src/main/java/org/example/competition22/data/RootResponse.java
@@ -4,4 +4,7 @@ public class RootResponse {
     public final String apiversion = "1";
     public final String author = "Team Jawa";
     public final String version = "0.1";
+    public final String color = "#00ff00";
+    public final String head = "smile";
+    public final String tail = "hook";
 }
